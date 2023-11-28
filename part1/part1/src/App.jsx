677944c579dfc21eefe95a3fa9cce529418c8632
dekuu5 <deku332211@gmail.com>
2023-11-28 19:49:41 +0200

    1.5

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,40 +1,99 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
-
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from "react"
 
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
+// function Counter({ counter }){
+//   return (
+//     <div>
+//       {
+//         counter
+//       }
+//     </div>
+//   )
+// }
+// function Button({ f , text}){
+//   return(
+//     <div>
+//       <button onClick={f}>{text}</button>
+//     </div>
+//   )
+// }
+
+// function App(){
+//   let [counter, setCounter] = useState(0);
+//   const incremnet = () => setCounter(counter+1);
+//   const reset = () => setCounter(0);
+//   console.log('rendering...', counter)
+//   return(
+//     <div>
+//       <Counter counter={counter} />
+//       <Button f={incremnet} text={"add 1"}/>
+//       <Button f={reset} text={"reset "}/>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={() => setLeft(left + 1)}>
+//         left
+//       </button>
+//       <button onClick={() => setRight(right + 1)}>
+//         right
+//       </button>
+//       {right}
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+  
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {right}
+//       <p>{allClicks.join(' ')}</p>
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      <p>{friends[0]}</p>
-      <p>{friends[1]}</p>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
+
 
 export default App
