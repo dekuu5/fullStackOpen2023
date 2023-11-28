a5aafaa5dd7dbef49ed726905e07f10b607292e0
dekuu5 <deku332211@gmail.com>
2023-11-28 20:20:27 +0200

    1.7

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -26,16 +26,33 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0);
+  const [avg, setAvg] = useState(0.0);
+  const [positve, setPositve] = useState(0);
 
+  function calculte(good, neutral, bad){
+    setAll(all +1)
+    let average = ((good * 1) + (bad * -1))/(all+1)
+    console.log(average)
+    setAvg(average)
+    let positive = (good*100/(all+1))
+    console.log(positive)
+    setPositve(positive)
+  }
   function incGood() {
     setGood(good + 1)
+    calculte(good+1, neutral, bad)
   }
   function incNeutral() {
     setNeutral(neutral + 1)
+    calculte(good, neutral+1, bad)
   }
   function incBad() {
     setBad(bad + 1)
+    calculte(good ,neutral, bad+1)
   }
+
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -46,6 +63,9 @@ const App = () => {
       <Display val={good} text="good" />
       <Display val={neutral} text="neutral" />
       <Display val={bad} text="bad" />
+      <Display val={avg} text="average" />
+      <Display val={all} text="all" />
+      <Display val={positve} text="positive" />
     </div>
   )
 }
